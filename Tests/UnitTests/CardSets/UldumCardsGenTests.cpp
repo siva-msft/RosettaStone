6d5b48d2c7dd5ac1da15877cab62d624921accd6
1cekrim <1cekrim.dev@gmail.com>
2020-01-18 15:16:00 +0900

    feat(card-impl): Implement card 'Beaming Sidekick' (ULD_191)

--- Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
@@ -6,7 +6,61 @@
 
 #include <Utils/CardSetUtils.hpp>
 
-TEST(UldumCardsGen, Temp)
+#include <Rosetta/Actions/Draw.hpp>
+#include <Rosetta/Cards/Cards.hpp>
+#include <Rosetta/Zones/FieldZone.hpp>
+
+using namespace RosettaStone;
+using namespace PlayerTasks;
+using namespace SimpleTasks;
+
+// --------------------------------------- MINION - NEUTRAL
+// [ULD_191] Beaming Sidekick - COST:1 [ATK:1/HP:2]
+// - Set: Uldum, Rarity: Common
+// --------------------------------------------------------
+// Text: <b>Battlecry:</b> Give a friendly minion +2 Health.
+// --------------------------------------------------------
+// GameTag:
+// - BATTLECRY = 1
+// --------------------------------------------------------
+// PlayReq:
+// - REQ_TARGET_IF_AVAILABLE = 0
+// - REQ_FRIENDLY_TARGET = 0
+// - REQ_MINION_TARGET = 0
+// --------------------------------------------------------
+TEST(NEUTRALUldumTest, ULD_191_BeamingSidekick)
 {
-    EXPECT_TRUE(true);
+    GameConfig config;
+    config.player1Class = CardClass::MAGE;
+    config.player2Class = CardClass::MAGE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_START);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Beaming Sidekick"));
+    const auto card2 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Flame Imp"));
+    const auto card3 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Flame Imp"));
+
+    game.Process(curPlayer, PlayCardTask::Minion(card2));
+    game.Process(curPlayer, PlayCardTask::Minion(card3));
+    game.Process(curPlayer, PlayCardTask::MinionTarget(card1, card2));
+    EXPECT_EQ(curField[0]->GetHealth(), 4);
+    EXPECT_EQ(curField[1]->GetHealth(), 2);
+    EXPECT_EQ(curField.GetCount(), 3);
 }
